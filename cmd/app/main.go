f9478f533e004c54885c126bd137896c4edb205d
melegattip <melegattip@gmail.com>
2025-03-18 12:59:26 -0300

    feat: configuración principal de la aplicación

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,25 +3,31 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/melegattip/financial-resume-engine/internal/categories"
-	"github.com/melegattip/financial-resume-engine/internal/reports"
+
+	"github.com/melegattip/financial-resume-engine/internal/config"
 	"github.com/melegattip/financial-resume-engine/internal/transactions"
 )
 
 func main() {
 	router := gin.Default()
 
+	// Inicializar la base de datos
+	db := config.InitDB()
+
+	// Inicializar handlers
+	transactionHandler := transactions.NewTransactionHandler(db)
+
 	// Rutas para transacciones
-	router.POST("/api/v1/transactions", transactions.CreateTransaction)
-	router.GET("/api/v1/transactions", transactions.HandleListTransactions)
-	router.PATCH("/api/v1/transactions/:transaction_id", transactions.HandleUpdateTransaction)
+	router.POST("/api/v1/transactions", transactionHandler.HandleCreateTransaction)
+	router.GET("/api/v1/transactions", transactionHandler.HandleListTransactions)
+	//router.PATCH("/api/v1/transactions/:transaction_id", transactionHandler.HandleUpdateTransaction)
 
 	// Rutas para categorías
-	router.POST("/api/v1/categories", categories.CreateCategory)
-	router.GET("/api/v1/categories", categories.ListCategories)
+	//router.POST("/api/v1/categories", categories.CreateCategory)
+	//router.GET("/api/v1/categories", categories.ListCategories)
 
 	// Rutas para reportes
-	router.GET("/api/v1/reports/financial", reports.GenerateFinancialReport)
+	//router.GET("/api/v1/reports/financial", reports.GenerateFinancialReport)
 
 	router.Run(":8080")
 }
